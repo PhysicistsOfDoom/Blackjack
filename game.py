85c1e25428694e7c80c46f3aa400c48d1ba37b0e
PhysicistsOfDoom <corbin.beus@gmail.com>
2024-11-13 21:01:44 -0700

    Created game.py to set up classes and resource functions

---
+++ game.py
@@ -0,0 +1,10 @@
+import os
+
+#Load the images with resource functions
+def load_cards():
+    pass
+
+
+#Classes for game functionality
+class Deck:
+    pass
--- main.py
+++ main.py
@@ -1,54 +1,56 @@
 import pygame
 from sys import exit
 
-#Initializer, Display, Caption, Clock
-pygame.init()
-screen = pygame.display.set_mode((800, 400)) #Display Surface
-pygame.display.set_caption("Blackjack") #Screen title
-fps = pygame.time.Clock()
+def main():
+    #Initializer, Display, Caption, Clock
+    pygame.init()
+    screen = pygame.display.set_mode((800, 400)) #Display Surface
+    pygame.display.set_caption("Blackjack") #Screen title
+    fps = pygame.time.Clock()
 
 
-# Varaibles
-sky_surface = pygame.Surface((800, 350)) #regular surface to blit on the display surface
-sky_surface.fill('Blue')
+    # Varaibles
+    sky_surface = pygame.Surface((800, 350)) #regular surface to blit on the display surface
+    sky_surface.fill('Blue')
 
-ground = pygame.Surface((800, 50)) #ground regular surface
-ground.fill("Green")
+    ground = pygame.Surface((800, 50)) #ground regular surface
+    ground.fill("Green")
 
-test_font = pygame.font.Font(None, 50) #text to display 
-text_surface = test_font.render("Hello", False, "White") #rendering the text
+    test_font = pygame.font.Font(None, 50) #text to display 
+    text_surf = test_font.render("Hello", False, "White") #rendering the text
+    text_rect = text_surf.get_rect(center=(400, 50))
 
-player = pygame.Surface((50,50)) #regular surface player
-player.fill("Black")
-player_rect = player.get_rect(midbottom = (400, 350)) #wrapping the player surface in a rect to give it easier function and perks
+    player = pygame.Surface((50,50)) #regular surface player
+    player.fill("Black")
+    player_rect = player.get_rect(midbottom = (400, 350)) #wrapping the player surface in a rect to give it easier function and perks
 
-threat_surf = pygame.Surface((50, 50)) #Threat regular surface to test collision
-threat_surf.fill("White")
-threat_rect = threat_surf.get_rect(midbottom = (100, 350)) #
-
-
-while True:
-    #check input events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() #stops the pygame modules
-            exit() #closes the loop
+    threat_surf = pygame.Surface((50, 50)) #Threat regular surface to test collision
+    threat_surf.fill("White")
+    threat_rect = threat_surf.get_rect(midbottom = (100, 350)) #
 
 
-    screen.blit(sky_surface, (0,0)) #blitting the test_surface onto the display surface
-    screen.blit(ground, (0, 350)) #blitting the ground surface onto the floor
-    screen.blit(text_surface, (400,50))
-    screen.blit(player, player_rect)
-    if player_rect.right > 0:
-        player_rect.right -= 5
-    else:
-        player_rect.right = 800
-
-    screen.blit(threat_surf, threat_rect)
-    threat_rect.left += 1
-
-    
-    pygame.display.update() #updates the screen after all input events have changed.
-    fps.tick(60) #frame every 1/60 second.
+    while True:
+        #check input events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit() #stops the pygame modules
+                exit() #closes the loop
+            if event.type == pygame.MOUSEMOTION:
+                if player_rect.collidepoint(event.pos): 
+                    player.fill("Red")
 
 
+        screen.blit(sky_surface, (0,0)) #blitting the test_surface onto the display surface
+        screen.blit(ground, (0, 350)) #blitting the ground surface onto the floor
+        screen.blit(text_surf, text_rect)
+        screen.blit(player, player_rect)
+
+        screen.blit(threat_surf, threat_rect)
+
+        
+        pygame.display.update() #updates the screen after all input events have changed.
+        fps.tick(60) #frame every 1/60 second.
+
+
+if __name__ == "__main__":
+    main()
