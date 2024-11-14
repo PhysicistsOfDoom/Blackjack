3e3bf26022cafa5b7b6dbf0436e1ebd18f9797da
PhysicistsOfDoom <corbin.beus@gmail.com>
2024-11-14 10:09:03 -0700

    Added game.py, table.py & Test cases for terminal gameplay, just before we go into pygame.

--- game.py
+++ game.py
@@ -1,10 +1,48 @@
-import os
+from sys import exit
+from table import Deck, Dealer, Player
+"""
+- This module is where we handle all the functionalities in the U.I, pygame will handle the visuals
+- We will use the classes from the table module to handle the game logic
+"""
 
-#Load the images with resource functions
-def load_cards():
-    pass
+def main():
+    deck = Deck() #List of tuples of random cards
+    
+    #Initialize players
+    player = Player()
+    dealer = Dealer()
 
+    #Begin Game (Y/N)
+    begin = input("Want to begin? (Y/N)")
+    if begin.lower() == "y":
+        running = True
+    else:
+        exit()
+    
+    #Game Loop
+    while running:
+        #Deal cards to player and dealer
+        for _ in range(2):
+            player.recieve_card(deck.deal_cards())
+            dealer.recieve_card(deck.deal_cards())
 
-#Classes for game functionality
-class Deck:
-    pass
+        #Show cards to table
+        print(f'Player: {player.hand}\n Player Score: {player.calculate_score()}' )
+        print(f'Dealer: {dealer.hand}\n Dealer Score: {dealer.calculate_score()}')
+
+        #Ask to Hit or Stand. If hit, give player a new card
+        player_decision = input("Hit or Stand? (Y/N)")
+        if player_decision.lower() == "y":
+            player.recieve_card(deck.deal_cards())
+
+        #Automatically hit for dealer if score is less than 17
+        dealer.must_hit(deck.deal_cards())
+
+        #Recalculate scores
+        print(f'Player: {player.hand}\n Player Score: {player.calculate_score()}' )
+        print(f'Dealer: {dealer.hand}\n Dealer Score: {dealer.calculate_score()}')
+
+        running = False
+
+if __name__ == "__main__":
+    main()
---
+++ tests/dealer_test.py
@@ -0,0 +1,3 @@
+import pytest
+from table import Dealer
+
